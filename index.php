a76706d13e8e076404bf38b4e7b96396bdde2066
husainakhter <husainakhter@gmail.com>
2019-08-01 08:45:30 +0530

    phpmailer script

--- index.php
+++ index.php
@@ -6,16 +6,25 @@ $response = (object)array("status"=> "", "message"=>"");
 require('phpmailer/class.phpmailer.php');
 
 
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->SMTPDebug = 0;
-$mail->SMTPAuth = TRUE;
-$mail->SMTPSecure = "ssl";
-$mail->Port     = 465;  
+$mail = new PHPMailer(true);
+$mail->SMTPSecure = "tls";
+$mail->Port     = 587;  
 $mail->Username = "infotestguident077@gmail.com";
 $mail->Password = "GreatIndia@123";
 $mail->Host     = "smtp.gmail.com";
 $mail->Mailer   = "smtp";
+$mail->IsSMTP();
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = true;
+$mail->SMTPKeepAlive = true;  
+$mail->CharSet = 'utf-8'; 
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
 $mail->SetFrom($_POST['email'],$_POST['firstname']);
 $mail->AddReplyTo($_POST['email'], $_POST['firstname']);
 $mail->AddAddress("husainakhter@gmail.com");	
